afaed972d61450fd5eab9d372c0d8d92f2617cd1
yong6675 <ngjy-wm17@student.tarc.edu.my>
2019-10-21 13:25:51 +0800

    p1

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -27,29 +27,50 @@ class MainActivity : AppCompatActivity() {
             Toast.LENGTH_SHORT).show()*/
 
         val resultText: TextView = findViewById(R.id.result_text)
+        val resultText1: TextView = findViewById(R.id.result_text1)
+        val resultText2: TextView = findViewById(R.id.result_text2)
         /*resultText.text = "Dice Rolled!"*/
         val randomInt = Random().nextInt(6) + 1
+        val randomInt1 = Random().nextInt(6) + 1
+        val randomInt2 = Random().nextInt(6) + 1
         resultText.text = randomInt.toString()
+        resultText1.text = randomInt1.toString()
+        resultText2.text= randomInt2.toString()
+        /*val onscreenText = resultText.text.toString().toIntOrNull()*/
 
     }
 
     private fun countUP() {
         val resultText: TextView = findViewById(R.id.result_text)
+        val resultText1: TextView = findViewById(R.id.result_text1)
+        val resultText2: TextView = findViewById(R.id.result_text2)
 
-        if (resultText.text == "Hello world!") {
+        if (resultText.text == "Hello world!" || resultText1.text == "Hello world!" || resultText2.text == "Hello world!") {
             resultText.text = "1"
+            resultText1.text = "1"
+            resultText2.text = "1"
         } else {
             var resulttext1 = resultText.text.toString().toInt()
+            var resulttext2 = resultText1.text.toString().toInt()
+            var resulttext3 = resultText2.text.toString().toInt()
 
-            if (resulttext1 < 6) {
+            if (resulttext1 < 6 && resulttext2 < 6 && resulttext3 < 6) {
                 resulttext1++
+                resulttext2++
+                resulttext3++
                 resultText.text = resulttext1.toString()
+                resultText1.text = resulttext2.toString()
+                resultText2.text = resulttext3.toString()
             }
         }
     }
 
     private fun resetNum(){
         val resultText: TextView = findViewById(R.id.result_text)
+        val resultText1: TextView = findViewById(R.id.result_text1)
+        val resultText2: TextView = findViewById(R.id.result_text2)
         resultText.text = "0"
+        resultText1.text = "0"
+        resultText2.text = "0"
     }
 }
